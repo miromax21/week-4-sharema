f71679409af70f142be378ff6ce071af98dcd282
miromax <miromax21@gmail.com>
2019-04-19 19:57:57 +0400

    Initial Commit

---
+++ week-4-sharema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week-4-sharema
+//
+//  Created by maxim mironov on 19/04/2019.
+//  Copyright © 2019 maxim mironov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
